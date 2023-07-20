0ffd4f7aead978557a052ee68fd7ef5ab6b82008
Quazia <alf40k@gmail.com>
2023-07-19 21:58:40 -0400

    💡 generalize service in redirect controller

--- src/redirect/redirect.controller.ts
+++ src/redirect/redirect.controller.ts
@@ -3,6 +3,7 @@ import { SlugsService } from '../slugs/slugs.service';
 
 @Controller()
 export class RedirectController {
+  // TODO: This should be more generalized
   constructor(private readonly slugsService: SlugsService) {}
 
   @Get(':slug')
