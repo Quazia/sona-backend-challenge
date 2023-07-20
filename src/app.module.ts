8b916954345ebe8dac952f46812b4ddb96ebfc7e
Quazia <alf40k@gmail.com>
2023-07-20 14:15:41 -0400

    🔥 User module from template

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ScheduleModule } from '@nestjs/schedule';
-import { UsersModule } from './users/users.module';
+import { SlugsModule } from './slugs/slugs.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from './config';
@@ -19,7 +19,7 @@ import configuration from './config';
       inject: [ConfigService],
     }),
     ScheduleModule.forRoot(),
-    UsersModule,
+    SlugsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
