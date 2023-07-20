ef46d9b49e28ff128c9f4dcfbd588d279a2b8636
Quazia <alf40k@gmail.com>
2023-07-20 14:46:12 -0400

    ✅CreateSlugDto Should accept a valid URL

--- src/slugs/dto/create-slug.dto.spec.ts
+++ src/slugs/dto/create-slug.dto.spec.ts
@@ -4,20 +4,20 @@ import { CreateSlugDto } from './create-slug.dto';
 describe('CreateSlugDto', () => {
   it('Should accept a valid URL', async () => {
     const target: ValidationPipe = new ValidationPipe();
+    const urlObj = { url: 'https://www.google.com' };
     const metadata: ArgumentMetadata = {
       type: 'body',
       metatype: CreateSlugDto,
-      data: 'https://www.google.com',
     };
-    await expect(target.transform({} as CreateSlugDto, metadata)).resolves.toMatchObject({ url: 'https://www.google.com' });
+    await expect(target.transform(urlObj as CreateSlugDto, metadata)).resolves.toMatchObject(urlObj);
   });
   it('Should throw error on invalid URL', async () => {
     const target: ValidationPipe = new ValidationPipe();
+    const urlObj = { url: 'IS_THIS_A_URL?' };
     const metadata: ArgumentMetadata = {
       type: 'body',
       metatype: CreateSlugDto,
-      data: 'INVALID_URL',
     };
-    await expect(target.transform({} as CreateSlugDto, metadata)).rejects.toThrowError();
+    await expect(target.transform(urlObj as CreateSlugDto, metadata)).rejects.toThrowError();
   });
 });
