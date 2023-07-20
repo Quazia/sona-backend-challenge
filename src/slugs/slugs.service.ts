14a993bc7c4124cf12dcea2daf95a497c4dba4ed
Quazia <alf40k@gmail.com>
2023-07-20 14:33:51 -0400

    🚧 SlugsService uses entity vs model

--- src/slugs/slugs.service.ts
+++ src/slugs/slugs.service.ts
@@ -1,25 +1,26 @@
 import { Injectable } from '@nestjs/common';
-import { Slug } from './slug.model';
+import { Slug } from './entities/slug.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 @Injectable()
 export class SlugsService {
   // Temporary in-memory database
-  slugs: { [slugId: string]: Slug } = {};
-
-  create(url: string) {
+  constructor(@InjectRepository(Slug) private readonly slugsRepository: Repository<Slug>) {}
+  create(url: string): Promise<Slug> {
     // Generate slug
     // Push slug+URL to db
-    const slugId = 'eg041x';
-    const newSlug = new Slug(slugId, url, 0);
-    this.slugs[slugId] = newSlug;
-    return slugId;
+    // this.slugs[slugId] = newSlug;
+    return this.slugsRepository.save({ url });
   }
 
   read(slug: string): Slug {
-    return this.slugs[slug];
+    // return this.slugs[slug];
+    return null;
   }
 
   getRedirect(slug: string): string {
-    return this.slugs[slug].url;
+    // return this.slugs[slug].url;
+    return null;
   }
 }
 
