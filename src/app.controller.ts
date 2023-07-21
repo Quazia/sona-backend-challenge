a09363930bc81bb09cba342453fcd66f4f0dd907
Quazia <alf40k@gmail.com>
2023-07-20 22:10:08 -0400

    🎨 Global cleanup imports

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,9 @@
-import { Body, Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Req, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
 
   @Get('/health-check')
   healthCheck(): string {
--- src/stats/stats.controller.ts
+++ src/stats/stats.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Put, Body, Param } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 
 import { StatsService } from './stats.service';
 import { GetStatsDto } from './dto/get-stats.dto';
