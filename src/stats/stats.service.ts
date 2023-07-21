98e703ebe07bd2ba95bead225c173eca540d476a
Quazia <alf40k@gmail.com>
2023-07-20 20:29:42 -0400

    ✅StatsService getStats test cases

--- src/stats/stats.service.ts
+++ src/stats/stats.service.ts
@@ -1,6 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { Slug } from '../slugs/entities/slug.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class StatsService {
-  async getStats(skip: number, limit: number) {}
+  constructor(@InjectRepository(Slug) private readonly slugsRepository: Repository<Slug>) {}
+
+  async getStats(skip: number, limit: number) {
+    const [list, count] = await this.slugsRepository.findAndCount({
+      skip,
+      take: limit,
+    });
+    return { list, count };
+  }
 }
